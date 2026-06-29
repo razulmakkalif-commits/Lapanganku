3b4083e5f6fd2ae81a4a29badfdbccba169ba236
razulabcd <razulmakkalif@gmail.com>
2026-06-29 13:54:25 +0800

    modif

--- riwayat.php
+++ riwayat.php
@@ -138,7 +138,7 @@ $data = mysqli_query(
                     }elseif($row['status'] == 'Disetujui'){
                         echo "<span class='status disetujui'>Disetujui</span>";
                     }elseif($row['status'] == 'Ditolak'){
-                        echo "<span class='status ditolak'>Ditolak</span>";
+                        echo "<span class='status dibatalkan'>Dibatalkan</span>";
                     }else{
                         echo "<span class='status dibatalkan'>Dibatalkan</span>";
                     }
